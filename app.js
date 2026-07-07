ccce5a2dd3886c0667211d59421e2e7762921768
Diego Guerra <dguerra@autodoc.local>
2026-07-06 23:00:23 -0300

    atualizar quantidade de numeros

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumeroSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentaivas = 1;
 
@@ -18,7 +18,7 @@ responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
 
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', "Escolha um número entre 1 e 10");
+exibirTextoNaTela('p', "Escolha um número entre 1 e 100");
 }
 
 exibirMensagemInicial();
